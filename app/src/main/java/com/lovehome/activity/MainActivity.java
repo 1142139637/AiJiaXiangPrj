43d608ef3da6c7fe2cd116d48d62143704863786
xingfudechengzhang <3397365808@qq.com>
2016-07-17 18:09:44 +0800

    fx

--- app/src/main/java/com/lovehome/activity/MainActivity.java
+++ app/src/main/java/com/lovehome/activity/MainActivity.java
@@ -3,11 +3,8 @@ package com.lovehome.activity;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 
