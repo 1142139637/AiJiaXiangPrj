cec667abf06b402fafa5f4f4cb27ca8c2507182d
xingfudechengzhang <3397365808@qq.com>
2016-07-18 22:30:54 +0800

    fx

--- app/src/main/java/com/lovehome/activity/myview/Issue.java
+++ app/src/main/java/com/lovehome/activity/myview/Issue.java
@@ -4,12 +4,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.lovehome.R;
+import com.lovehome.activity.BaseActivity;
 
-public class Issue extends AppCompatActivity {
+public class Issue extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_issue);
+
+    }
+
+    @Override
+    protected void initView() {
+        super.initView();
+        findViewById(R.id.aitv);
     }
 }
