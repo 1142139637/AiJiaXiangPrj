07857c8ccff2d3cccb68f85baa1c00de3ec4d292
xingfudechengzhang <3397365808@qq.com>
2016-07-18 17:00:02 +0800

    fx

--- app/src/main/java/com/lovehome/activity/myview/AlterPassword.java
+++ app/src/main/java/com/lovehome/activity/myview/AlterPassword.java
@@ -1,15 +1,32 @@
 package com.lovehome.activity.myview;
-
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
 import com.lovehome.R;
+import android.os.Bundle;
+import android.app.Activity;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class AlterPassword extends AppCompatActivity {
+public class AlterPassword extends Activity  {
+
+    private TextView forgetpassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alter_password);
+
+        forgetpassword = (TextView) findViewById(R.id.forgetpassword);
+    }
+
+    private EditText getOldpassword(){
+        return (EditText) findViewById(R.id.oldpassword);
+    }
+
+    private EditText getNewonepwd(){
+        return (EditText) findViewById(R.id.newonepwd);
+    }
+
+    private EditText getNewtwopwd(){
+        return (EditText) findViewById(R.id.newtwopwd);
     }
 }
+
