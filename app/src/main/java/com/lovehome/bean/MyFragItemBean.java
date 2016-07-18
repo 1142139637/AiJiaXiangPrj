2318b88a6cf15b4899510e90febec137dabd7c8a
xingfudechengzhang <3397365808@qq.com>
2016-07-18 11:49:39 +0800

    fx

---
+++ app/src/main/java/com/lovehome/bean/MyFragItemBean.java
@@ -0,0 +1,25 @@
+package com.lovehome.bean;
+
+/**
+ * Created by Administrator on 2016/7/18.
+ */
+public class MyFragItemBean {
+    public int img ;//图片地址
+    public String content ;//文字内容
+
+    public int getImg() {
+        return img;
+    }
+
+    public void setImg(int img) {
+        this.img = img;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
