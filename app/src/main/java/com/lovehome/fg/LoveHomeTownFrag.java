f1041c59d69276d1845a7ab602ca2c8c5d0bad08
1142139637 <1142139637@qq.com>
2016-07-18 16:58:03 +0800

    pengdan

--- app/src/main/java/com/lovehome/fg/LoveHomeTownFrag.java
+++ app/src/main/java/com/lovehome/fg/LoveHomeTownFrag.java
@@ -36,6 +36,7 @@ public class LoveHomeTownFrag extends BaseFragment {
                 view.loadUrl(url);
                 return true;
 
+
                 //https://github.com/1142139637/AiJiaXiangPrj.git
 
 
