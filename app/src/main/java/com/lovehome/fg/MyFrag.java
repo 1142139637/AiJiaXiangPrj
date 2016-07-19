f8fa6c880048362a529a4f7aef8556c2af6297c4
1142139637 <1142139637@qq.com>
2016-07-19 16:45:20 +0800

    pd——分享

--- app/src/main/java/com/lovehome/fg/MyFrag.java
+++ app/src/main/java/com/lovehome/fg/MyFrag.java
@@ -31,6 +31,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.zip.Inflater;
 
+import cn.sharesdk.framework.ShareSDK;
+import cn.sharesdk.onekeyshare.OnekeyShare;
+
 public class MyFrag extends BaseFragment {
 
     private ImageView myFragImgId;
@@ -122,7 +125,7 @@ public class MyFrag extends BaseFragment {
                         startActivity(new Intent(getActivity(),RegardingLoveHometown.class));
                         break;
                     case 7://分享软件
-
+                        share();
                         break;
                     case 8://修改密码
                         startActivity(new Intent(getActivity(),AlterPassword.class));
@@ -154,6 +157,36 @@ public class MyFrag extends BaseFragment {
                 }
             }
         });
+    }
+
+    //分享
+    private void share() {
+            ShareSDK.initSDK(getActivity());
+            OnekeyShare oks = new OnekeyShare();
+            //关闭sso授权
+            oks.disableSSOWhenAuthorize();
+
+// 分享时Notification的图标和文字  2.5.9以后的版本不调用此方法
+            //oks.setNotification(R.drawable.ic_launcher, getString(R.string.app_name));
+            // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
+            oks.setTitle("爱家乡");
+            // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
+            oks.setTitleUrl("http://baidu.cn");
+            // text是分享文本，所有平台都需要这个字段
+            oks.setText("老乡见老乡，两眼。。。。");
+            // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+            //oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+            // url仅在微信（包括好友和朋友圈）中使用
+//            oks.setUrl("http://sharesdk.cn");
+            // comment是我对这条分享的评论，仅在人人网和QQ空间使用
+//            oks.setComment("呵呵");
+            // site是分享此内容的网站名称，仅在QQ空间使用
+//            oks.setSite(getString(R.string.app_name));
+            // siteUrl是分享此内容的网站地址，仅在QQ空间使用
+//            oks.setSiteUrl("http://sharesdk.cn");
+
+// 启动分享GUI
+            oks.show(getActivity());
     }
 
 
